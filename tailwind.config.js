cf7726f1ad1015fc14c708d8210938d39aa1657f
nitesh agnihotri <niteshagni1830@gmail.com>
2022-04-14 18:01:59 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        fontFamily: {
+          'Khula': ['Khula', 'sans-serif'],
+          'Varela': ['Varela', 'sans-serif'],
+          'Varela Round': ['Varela Round', 'sans-serif'],
+        },
+    },
+  },
+  plugins: [],
+}
